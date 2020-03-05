58e32e4dc679a941d5530881e235fe3f58f1d767
Jiajiaju <jiajujia@insweetion-inc.com>
2020-03-05 20:07:27 +0800

    feat: 更新 readme 文件，添加代码模版文件

---
+++ template/new-script.js
@@ -0,0 +1,20 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+    },
+
+    // onLoad(){},
+
+    // onDestroy(){},
+
+    // onEnable(){},
+
+    // onDisable(){},
+
+    // start(){},
+
+    // update(dt){},
+
+    // lateUpdate(dt){},
+});
---
+++ template/new-script.ts
@@ -0,0 +1,29 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    // public onLoad(): void
+    // {}
+
+    // public onDestroy(): void
+    // {}
+
+    // public onEnable(): void
+    // {}
+
+    // public onDisable(): void
+    // {}
+
+    // public start(): void
+    // {}
+
+    // public update(dt): void
+    // {}
+
+    // public update(dt: number): void
+    // {}
+
+    // public lateUpdate(dt: number): void
+    // {}
+}
